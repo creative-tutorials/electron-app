af29c1f6d975c2f5b162b79de06119faf0d1b9b0
Treasure Alekhojie <treasurealekhojie@yahoo.com>
2024-05-13 14:24:11 +0100

    feat: Integrated @electron-forge/cli for bundling and building application distribution feat: Implemented clipboard functionality, desktop notification, and updated application menu bar
    Signed-off-by: Treasure Alekhojie <treasurealekhojie@yahoo.com>

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { app, BrowserWindow } from "electron";
-import { Menu } from "electron";
-import { dialog } from "electron";
+import { Notification, clipboard, Menu, dialog } from "electron";
+import path from "node:path";
 
 const menuTemplate = [
   {
@@ -11,6 +11,21 @@ const menuTemplate = [
         click: () => {
           app.quit();
         },
+      },
+    ],
+  },
+
+  {
+    label: "View",
+    submenu: [
+      {
+        role: "reload",
+      },
+      {
+        role: "forceReload",
+      },
+      {
+        role: "toggleDevTools",
       },
     ],
   },
@@ -38,23 +53,74 @@ const menuTemplate = [
                   resolve();
                 }, 5000);
               });
+              const notificationTitle = "My Notification 🔔";
+              console.count("Start Notify");
+              new Notification({
+                actions: ["Action 1", "Action 2"],
+
+                icon: "./src/assets/icon.png",
+                title: notificationTitle,
+                body: "This is a sample notification.",
+                renotify: true,
+                tag: "test",
+              }).show();
+            });
+        },
+      },
+      {
+        label: "About",
+        click: () => {
+          dialog
+            .showMessageBox({
+              type: "info",
+              message: "About Electron App",
+              detail: "Made with Electron",
+              buttons: ["OK", "Cancel", "Copy"],
+            })
+            .then((response) => {
+              if (response.response === 2) {
+                try {
+                  clipboard.writeText("Made with Electron");
+
+                  console.log(clipboard.readText());
+                } catch (error) {
+                  console.error(error);
+                }
+              }
             });
         },
       },
     ],
   },
 ];
+
 const menu = Menu.buildFromTemplate(menuTemplate);
+
+const __dirname = path.resolve(path.dirname("./"));
 
 const createWindow = () => {
   const win = new BrowserWindow({
+    icon: path.join(__dirname, "src/assets/icon.png"),
     width: 800,
     height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
 
-  win.loadFile("index.html");
+  win.loadFile(path.join(__dirname, "index.html"));
 };
 app.whenReady().then(() => {
   createWindow();
   Menu.setApplicationMenu(menu);
+
+  app.on("activate", () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
 });
